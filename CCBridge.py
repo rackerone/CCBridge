3f68005cd73353acb2ab93ffa57f2430e55d6e7b
Aaron <asmith.technology@gmail.com>
2013-07-09 20:30:27 -0500

    compatibility blurb
    tested ccbridge.py with python 2.7.3 and pyrax 1.4.6

--- CCBridge.py
+++ CCBridge.py
@@ -4,6 +4,10 @@
 # -*- coding: utf-8 -*-
 #Copyright 2013 Aaron Smith
 
+#Tested with:
+#  python 2.7.3
+#  pyrax 1.4.6
+
 #Licensed under the Apache License, Version 2.0 (the "License");
 #you may not use this file except in compliance with the License.
 #You may obtain a copy of the License at
